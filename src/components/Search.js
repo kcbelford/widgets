5e1fc84db9fb82ce3069d25c15f95b93026b480d
kcbelford <kcbelford@hotmail.com>
2020-11-16 21:11:29 -0800

    add useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect (() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php?', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  });
 
   return (
     <div>
@@ -15,6 +48,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
